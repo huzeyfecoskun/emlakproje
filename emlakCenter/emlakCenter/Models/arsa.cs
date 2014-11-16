ace47114a918ded35a0246ab8da8d5c5a9f42832
huzeyfecoskun <huzeyfecoskun@hotmail.com>
2014-11-16 21:40:33 +0200

    huzeyfe coşkun
    Clasları oluşturdum

---
+++ emlakCenter/emlakCenter/Models/arsa.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace emlakCenter.Models
+{
+    public class arsa
+    {
+            public int Id { get; set; }
+            //KONUM
+            public int il { get; set; }
+            public int ilce { get; set; }
+            public int semt { get; set; }
+
+            public int metrekare { get; set; }
+
+            public int fiyat { get; set; }
+
+            public int tapuDurumu { get; set; }
+
+            public int ilanSahibi { get; set; }
+
+            public bool hasResim { get; set; }
+            public bool hasHarita { get; set; }
+            public bool hasVideo { get; set; }
+
+            public DateTime ilantarihi { get; set; }
+    }
+
+    public class il
+    {
+        public int Id { get; set; }
+        public string il_adi { get; set; }
+    }
+
+    public class ilce
+    {
+        public int Id { get; set; }
+        public int il_id { get; set; }
+        public string ilce_adi { get; set; }
+    }
+
+    public class semt
+    {
+        public int Id { get; set; }
+        public int ilce_id { get; set; }
+        public string semt_adi { get; set; }
+    }
+
+    public class tapuDurumu
+    {
+        public int Id { get; set; }
+        public string durum { get; set; }
+    }
+
+    public class ilanSahibi
+    {
+        public int Id { get; set; }
+        public string ilanSahibi_adi { get; set; }
+    }
+}
