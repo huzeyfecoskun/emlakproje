4b0882725eac6f84f998a2b4984933c02a19c5db
halidsenyigit <halidsenyigit@gmail.com>
2014-11-23 20:09:12 +0200

    s

--- emlakCenter/emlakCenter/App_Start/BundleConfig.cs
+++ emlakCenter/emlakCenter/App_Start/BundleConfig.cs
@@ -11,7 +11,7 @@ namespace emlakCenter
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jqueryeval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/unob").Include(
