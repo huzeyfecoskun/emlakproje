a2857b6ec1be77a74183d4dd7612bb49949c3e16
halidsenyigit <halidsenyigit@gmail.com>
2014-11-23 19:03:12 +0200

    ss

--- emlakCenter/emlakCenter/App_Start/BundleConfig.cs
+++ emlakCenter/emlakCenter/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace emlakCenter
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/unob").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +29,7 @@ namespace emlakCenter
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
