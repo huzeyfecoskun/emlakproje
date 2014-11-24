672a88746a62ac172fe1ce2e2e7da0c016eb7b69
huzeyfecoskun@hotmail.com <huzeyfecoskun@hotmail.com>
2014-11-24 02:32:28 +0200

    database,  imageProcess

---
+++ emlakCenter/emlakCenter/Controllers/HelperController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Drawing;
+using emlakCenter.Models;
+using System.IO;
+
+namespace emlakCenter.Controllers
+{
+    public class HelperController : Controller
+    {
+        systemDB _db = new systemDB();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null) _db.Dispose();
+            base.Dispose(disposing);
+        }
+        // GET: Helper
+        public ActionResult resim(int id, int w, int h)
+        {
+            //Dosya adresi
+            var str = _db.ArsaMedya.Where(n => n.id == id).FirstOrDefault();
+            //Databaseden resmi çekelim
+            Image tmp = Image.FromFile(Server.MapPath(str.resim));
+            //width ve height ten bitmap oluşturalım
+            Bitmap bitmap = new Bitmap(w, h);
+            //Graphics oluştur
+            Graphics g = Graphics.FromImage((Image)bitmap);
+            //Yeni boyutlara yazdıralım
+            g.DrawImage(tmp, new Rectangle(0, 0, w, h));
+            //Memory stream e bitmapı yaz
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            //Resim olarak döndür
+            return base.File(ms.ToArray(), "image/png");
+        }
+    }
+}
---
+++ emlakCenter/emlakCenter/Models/systemDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace emlakCenter.Models
+{
+    public class systemDB : DbContext
+    {
+        //public systemDB()
+        //    : base("")
+        //{
+
+        //}
+
+        public DbSet<arsaMedya> ArsaMedya { get; set; }
+
+    }
+}
