41016233ca3f3a4a465f7f58206e895fe6cb7b09
halidsenyigit <halidsenyigit@gmail.com>
2014-11-30 21:17:17 +0200

    hld
    pazar akşam son

--- emlakCenter/emlakCenter/Controllers/HelperController.cs
+++ emlakCenter/emlakCenter/Controllers/HelperController.cs
@@ -24,7 +24,25 @@ namespace emlakCenter.Controllers
             //Dosya adresi
             var str = _db.ArsaMedya.Where(n => n.id == id).FirstOrDefault();
             //Databaseden resmi çekelim
-            Image tmp = Image.FromFile(Server.MapPath(str.resim));
+            Image tmp = Image.FromFile(Server.MapPath("~/Content/img/ilan-resim/resim-1.jpg"));
+            //width ve height ten bitmap oluşturalım
+            Bitmap bitmap = new Bitmap(w, h);
+            //Graphics oluştur
+            Graphics g = Graphics.FromImage((Image)bitmap);
+            //Yeni boyutlara yazdıralım
+            g.DrawImage(tmp, new Rectangle(0, 0, w, h));
+            //Memory stream e bitmapı yaz
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            //Resim olarak döndür
+            return base.File(ms.ToArray(), "image/png");
+        }
+
+
+        public ActionResult DosyaAdi(string adres, int w, int h)
+        {
+            //Databaseden resmi çekelim
+            Image tmp = Image.FromFile(Server.MapPath("~/Content/img/ilan-resim/"+adres));
             //width ve height ten bitmap oluşturalım
             Bitmap bitmap = new Bitmap(w, h);
             //Graphics oluştur
