fe64db0caa19b0ee6049b7eb59dd0f3e98232d61
halidsenyigit <halidsenyigit@gmail.com>
2014-12-03 01:42:48 +0200

    halid
    upload izin verilen uzantılar

--- emlakCenter/emlakCenter/Controllers/AdminController.cs
+++ emlakCenter/emlakCenter/Controllers/AdminController.cs
@@ -24,15 +24,29 @@ namespace emlakCenter.Controllers
                     HttpPostedFileBase file = Request.Files[i];
                     int fileSize = file.ContentLength;
                     string fileName = file.FileName;
-                    string[] arr = fileName.Split('\\');
-                    fileName = arr[arr.Length - 1];
-                    file.SaveAs(Server.MapPath("~/content/uploads/" + fileName));
-                    //hImage img = new hImage();
-                    //img.imageUrl = fileName;
-                    //_db.images.Add(img);
-                    //_db.SaveChanges();
+                    string[] ext = fileName.Split('.');
+                    string extention = ext[ext.Length - 1];
+                    if (
+                        extention == "jpg" ||
+                        extention == "png" ||
+                        extention == "jpeg" ||
+                        extention == "JPG" ||
+                        extention == "PNG" ||
+                        extention == "JPEG"
+                      )
+                    {
+                        string[] arr = fileName.Split('\\');
+                        fileName = arr[arr.Length - 1];
+                        file.SaveAs(Server.MapPath("~/Content/uploads/" + fileName));
+                        //hImage img = new hImage();
+                        //img.imageUrl = fileName;
+                        //_db.images.Add(img);
+                        //_db.SaveChanges();
+                        return Content("Success");
+                    }
+
                 }
-                return Content("Success");
+                
             }
             return Content("failed");
         }
