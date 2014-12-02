dd26b74fc322510590a67ccc8c9987023ac49f88
halidsenyigit <halidsenyigit@gmail.com>
2014-12-02 20:08:19 +0200

    halidsenyigit
    file upload hatası var henüz

--- emlakCenter/emlakCenter/Controllers/AdminController.cs
+++ emlakCenter/emlakCenter/Controllers/AdminController.cs
@@ -12,5 +12,26 @@ namespace emlakCenter.Controllers
         {
             return View();
         }
+
+
+        public ActionResult Upload(HttpPostedFile photo)
+        {
+            if (Request.Files.Count != 0)
+            {
+                for (int i = 0; i < Request.Files.Count; i++)
+                {
+                    HttpPostedFileBase file = Request.Files[i];
+                    int fileSize = file.ContentLength;
+
+                    string fileName = file.FileName;
+                    string[] arr = fileName.Split('\\');
+                    fileName = arr[arr.Length - 1];
+                    file.SaveAs(Server.MapPath("~/Content/Uploads/" + fileName));
+                }
+                return Content("Success");
+            }
+            return Content("failed");
+
+        }
     }
 }
