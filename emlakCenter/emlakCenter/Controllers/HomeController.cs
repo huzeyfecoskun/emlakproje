0b6d6b65baff6b986210c21ff06fa7bbaa953c80
mzyil <mzyil12@gmail.com>
2014-12-03 05:43:50 +0200

    controllerdan ajax ile sonuç bilgisi alıp içeriğe append et

--- emlakCenter/emlakCenter/Controllers/HomeController.cs
+++ emlakCenter/emlakCenter/Controllers/HomeController.cs
@@ -19,11 +19,7 @@ namespace emlakCenter.Controllers
             c.Value = EncryptDecryptUtils.Encrypt("halid", "asss", "ddddd", "SHA1");
             c.Expires = DateTime.Now.AddDays(1);
             Response.SetCookie(c);
-
-
             HttpContext.Session["ute"] = "aaa";
-
-
             return View();
         }
 
@@ -49,6 +45,39 @@ namespace emlakCenter.Controllers
         {
             var semt = _db.semtler.Where(n => n.ilce_id == id).ToList();
             return Json(JsonConvert.SerializeObject(semt), JsonRequestBehavior.AllowGet);
+        }
+
+        class SearchObject
+        {
+            public String imgUrl,content,price,area;
+        }
+
+        public ActionResult GetSearchResults(string queryString)
+        {
+            SearchObject s1 = new SearchObject();
+            s1.imgUrl = new Uri(Request.Url, Url.Content("~/Helper/DosyaAdi/?adres=resim-1.jpg&w=200&h=140")).ToString();
+            s1.content = "İçerik 1";
+            s1.price = "2.000.000 TRY";
+            s1.area = "1.000.000";
+
+            SearchObject s2 = new SearchObject();
+            s2.imgUrl = new Uri(Request.Url, Url.Content("~/Helper/DosyaAdi/?adres=resim-1.jpg&w=200&h=140")).ToString();
+            s2.content = "İçerik 2";
+            s2.price = "2.000.000 TRY";
+            s2.area = "1.000.000";
+
+            SearchObject s3 = new SearchObject();
+            s3.imgUrl = new Uri(Request.Url, Url.Content("~/Helper/DosyaAdi/?adres=resim-1.jpg&w=200&h=140")).ToString();
+            s3.content = "İçerik 3";
+            s3.price = "2.000.000 TRY";
+            s3.area = "1.000.000";
+
+            SearchObject[] result = new SearchObject[3];
+            result[0] = s1;
+            result[1] = s2;
+            result[2] = s3;
+
+            return Json(JsonConvert.SerializeObject(result), JsonRequestBehavior.AllowGet);
         }
 
     }
--- emlakCenter/emlakCenter/Scripts/site.js
+++ emlakCenter/emlakCenter/Scripts/site.js
@@ -1,5 +1,5 @@
-﻿var url = "localhost/emlakCenter";
-
+﻿//var url = "localhost/emlakCenter";
+var url = "localhost:26974"
 $(document).ready(function () {
 
     $("#ilSecimi").change(function () {
@@ -25,18 +25,63 @@ $(document).ready(function () {
         });
     });
     var end = new Date();
-        
-        $('.bas_time').datetimepicker({
-        format: 'dd.mm.yyyy',
-        weekStart: 1,
-        autoclose: 1,
-        startView: 2,
-        forceParse: 0,
-        language: 'tr',
-        minView: 2,
-        maxView: 2,
-        endDate: end,
-        pickerPosition: 'top-right'
-        });
+    $('.bas_time').datetimepicker({
+    format: 'dd.mm.yyyy',
+    weekStart: 1,
+    autoclose: 1,
+    startView: 2,
+    forceParse: 0,
+    language: 'tr',
+    minView: 2,
+    maxView: 2,
+    endDate: end,
+    pickerPosition: 'top-right'
+    });
+
+     var skeleton = '<div class="e-ilan">'+
+     '   <img src="[[url]]">'+
+     '   <div class="e-baslik">'+
+     '       [[content]]'+
+     '   </div>'+
+     '   <hr />'+
+     '   <div class="e-aciklama">'+
+     '       <div class="e-fiyat">'+
+     '           <span class="e-label">Fiyat</span> [[price]]'+
+     '       </div>'+
+     '       <div class="e-mkare">'+
+     '           <span class="e-label">Metrekare</span> [[area]] m<sup>2</sup>'+
+     '       </div>'+
+     '   </div>'+
+     '</div>';
+
+     $.get("http://" + url + "/home/getSearchResults", { queryString: "some-search-credentials" }, function (data) {
+         var results = $.parseJSON(data);
+         for (var i = 0 ; i < results.length ; i++) {
+             //var newSkeleton = skeleton;
+             var valuesOld = [
+                 "[[url]]",
+                 "[[content]]",
+                 "[[price]]",
+                 "[[area]]"
+             ];
+             var valuesNew = [
+                 results[i].imgUrl,
+                 results[i].content,
+                 results[i].price,
+                 results[i].area
+             ];
+             $("#e-right").append(replaceWithRespect2Index(valuesOld,valuesNew,skeleton));
+         }
+     });
 
 });
+
+function replaceWithRespect2Index(oldV, newV, oldText ){
+    var newText = oldText;
+    if (oldV.length == newV.length) {
+        for (var m = 0; m < oldV.length; m++) {
+            newText = newText.replace(oldV[m], newV[m]);
+        }
+    }
+    return newText;
+}
